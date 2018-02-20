05b894fb251201b2e01e9d9894b03e929091ee36
Natasha S Love <debora@inbox.ru>
2018-02-20 13:26:09 -0600

    HW05

--- CIS2013_Week05_Homework1.cpp
+++ CIS2013_Week05_Homework1.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 // Input of month and year
 void input(int month, int year);
+//Finding if it is a leap year
+bool IsLeapYear(const int year);
 
 int main (){
 	int m, y;
 	
 	input (m, y);
+	IsLeapYear (y);
+	
 	
 	
 	
@@ -38,4 +42,17 @@ void input(int month, int year) {
 	cout << "Enter the year " << endl;
 	cin >> y;
 	cout << endl;
+}
+bool IsLeapYear(const int year) {
+   return ((year%400==0) || (year%4==0 && year%100!=0));
+}
+int GetDaysInMonth (const int year, const int month) {
+   switch (month) {
+	    case 2: return IsLeapYear(year) ? 29 : 28;
+	    case 4:
+		case 6:
+		case 9:
+		case 11: return 30;
+		default: return 31;
+  }   
 }
