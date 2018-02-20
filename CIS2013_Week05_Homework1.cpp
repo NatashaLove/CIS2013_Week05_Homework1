fb093fc83f6473fc50d7c95f820e2b323992488a
Natasha S Love <debora@inbox.ru>
2018-02-20 13:06:42 -0600

    Initial commit

---
+++ CIS2013_Week05_Homework1.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+// Input of month and year
+void input(int month, int year);
+
+int main (){
+	int m, y;
+	
+	input (m, y);
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+return 0;
+}
+
+
+void input(int month, int year) {
+	using namespace std;
+	int m, y;
+	cout << endl;
+	cout << "Hello!!! " << endl;
+	cout << endl;
+	cout << "Enter the month "<< endl;
+	cin >> m;
+	cout << endl;
+	cout << "Enter the year " << endl;
+	cin >> y;
+	cout << endl;
+}
